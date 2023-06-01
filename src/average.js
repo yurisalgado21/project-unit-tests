005c97d3ee65f795d3c8edc1313f5a888a95d898
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-01 15:29:20 -0300

    Exercício 1 feito

--- src/average.js
+++ src/average.js
@@ -11,8 +11,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  let media = soma / array.length;
+  return Math.round(media);
 };
 
 module.exports = average;
