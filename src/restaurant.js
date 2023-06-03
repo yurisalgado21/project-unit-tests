29eba231976635e36bb05bb79e5d338027bf5307
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-06-03 14:06:07 -0300

    Corrgindo o erro do lint

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,12 +1,10 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = (menu) => {
-  return {
-    fetchMenu() {
-      return menu;
-    },
-  };
-};
+const createMenu = (menu) => ({
+  fetchMenu() {
+    return menu;
+  },
+});
 
 module.exports = createMenu;
